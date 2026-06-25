c8e5d9b9ff591b939978df805d42e94f929c0f11
Minh Đức <132887674+minhducn14@users.noreply.github.com>
2026-06-25 08:28:12 +0700

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const map = new Map();
+
+        for (let str of strs) {
+                let count = new Array(26).fill(0);
+                for (let i = 0; i < str.length; i++) {
+                        count[str.charCodeAt(i) - 97]++;
+                }
+                let key = count.join('#');
+                                                                                
+                if (!map.has(key)) {
+                        map.set(key, []);
+                }
+                map.get(key).push(str);  
+        }
+
+         return Array.from(map.values());
+
+    }
+}
