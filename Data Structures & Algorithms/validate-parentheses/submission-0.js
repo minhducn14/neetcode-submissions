6b9ac6c340e7cbdbee757412e0ee9561977093f1
Minh Đức <132887674+minhducn14@users.noreply.github.com>
2026-07-03 09:46:22 +0700

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const stack = [];
+        const map = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        };
+
+        for (let char of s) {
+            if (map[char]) {
+                const topElement = stack.length === 0 ? '#' : stack.pop();
+                
+                if (topElement !== map[char]) {
+                    return false;
+                }
+            } else{
+                stack.push(char);
+            }
+        }
+        return stack.length === 0;
+    }
+}
