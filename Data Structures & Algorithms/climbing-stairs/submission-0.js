01fd5ed6487ccd408f2d12e6fa75aa16bac847ae
Minh Đức <132887674+minhducn14@users.noreply.github.com>
2026-07-24 09:51:02 +0700

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n) {
+        if (n <= 2) return n;
+
+        let prev2 = 1;
+        let prev1 = 2;
+        let current = 0;
+
+        for (let i = 3; i <= n; i++) {
+            current = prev1 + prev2; 
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return current;
+    }
+}
